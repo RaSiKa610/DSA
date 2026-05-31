1bb35ced3c6ff5dfc25e4d4a0068c781edb5c320
Rasika <rasikashinde610gmail.com>
2026-05-31 21:38:29 +0530

    30-5-26

---
+++ Problem Solving/sequence_equation.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    result = []
+    
+    for x in range(1, len(p)+1):
+        index = p.index(x) + 1
+        y = p.index(index) +1
+        
+        result.append(y)
+        
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
