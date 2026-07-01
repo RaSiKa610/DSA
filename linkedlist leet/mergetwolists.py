12bdd991e00fbe6922124b111092eabc401b7378
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-07-01 23:21:58 +0530

    1-07-26

---
+++ linkedlist leet/mergetwolists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        res = ListNode(0)
+        r = res
+        while list1 and list2:
+            if list1.val <= list2.val:
+                r.next = list1
+                list1 = list1.next
+            else:
+                r.next = list2
+                list2 = list2.next
+            r = r.next
+        
+        if list1:
+            r.next = list1
+        elif list2:
+            r.next = list2
+        return res.next
