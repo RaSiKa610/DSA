0487ba482a48e5e900cad1c978fb78fe8a62a197
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-07-05 21:58:54 +0530

    5-07-26

---
+++ Tree leet/Zigzaglevel.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        res = []
+        q = deque([root])
+
+        while q:
+            size = len(q)
+            level = []
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(level)
+
+        for l in range(1, len(res), 2):
+            res[l] = res[l][::-1]
+
+        return res
