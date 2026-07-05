be322501d565ab316ddef3e097a6b1cff87dd05d
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-07-06 01:29:18 +0530

    5-07-26

---
+++ Tree leet/Getmindiff.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def getMinimumDifference(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        self.p = None
+        self.mini = float('inf')
+
+        def helper(node):
+            if not node:
+                return
+
+            helper(node.left)
+            if self.p is not None:
+                self.mini = min(self.mini, node.val - self.p)
+
+            self.p = node.val
+            helper(node.right)
+
+        helper(root)
+
+        return self.mini
+                
