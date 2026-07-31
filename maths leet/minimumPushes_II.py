402f958afedcf04f256088bbbd4759bb8356aa90
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-07-31 11:36:31 +0530

    31-07-26

---
+++ maths leet/minimumPushes_II.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        freq = Counter(word)
+
+        counts = sorted(freq.values(), reverse = True)
+
+        ans = 0
+        for i, f in enumerate(counts):
+            pushes = i//8 + 1
+            ans += f * pushes 
+
+        return ans
