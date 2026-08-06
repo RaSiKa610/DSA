e84286b0f1344b2e8b3a02b93ab06380c64eab7f
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-08-06 23:34:23 +0530

    06-08-26

---
+++ maths leet/smallestNumber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
+        while True:
+            product = 1
+            temp = n
+            while temp > 0:
+                product *=  temp % 10
+                temp //= 10
+
+            if product % t == 0:
+                return n
+
+            n += 1
