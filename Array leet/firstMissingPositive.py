aeb7586fcc8d0f46cb051262dbd855b56af2988f
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-08-11 21:54:43 +0530

    11-08-26

---
+++ Array leet/firstMissingPositive.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        for i in range(n):
+            # Place nums[i] at index nums[i] - 1 if it's in range [1, n]
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                correct_idx = nums[i] - 1
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
+        
+        # Find the first index where index + 1 != nums[i]
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+                
+        return n + 1
