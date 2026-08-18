a6f6ae96ec8953d478bd875f3cfacec82a26d050
Rasika Prasad Shinde <rasikashinde610@gmail.com>
2026-08-19 01:18:20 +0530

    18-08-26

---
+++ Array leet/largestInteger.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def largestInteger(self, nums, k):
+        count = {}
+
+        n = len(nums)
+
+        # Check every subarray of size k
+        for i in range(n - k + 1):
+            seen = set()
+
+            for j in range(i, i + k):
+                seen.add(nums[j])
+
+            # Each number is counted once for this subarray
+            for x in seen:
+                count[x] = count.get(x, 0) + 1
+
+        ans = -1
+
+        # Find largest number appearing in exactly one subarray
+        for x in count:
+            if count[x] == 1:
+                ans = max(ans, x)
+
+        return ans
